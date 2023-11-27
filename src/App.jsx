3c1373ac567939db4294bbf28d8ce3ce78dc7a6a
Jony Monroy <jonymonroy@gmail.com>
2023-11-27 09:55:16 -0500

    React Forms: Estructura básica de navegación entre formularios

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+import ForgotPassword from "./components/ForgotPassword"
+import Signin from "./components/Signin"
+import Signup from "./components/Signup"
+
+
+const App = () => {
+const [step, setStep] = useState('signin');
+  return  (
+    <div className="container">
+      { step === "signin" && <Signin />}
+      { step === "signup" && <Signup />}
+      { step === "forgot" && <ForgotPassword />}
+    </div>
+  )
+}
+
+export default App
